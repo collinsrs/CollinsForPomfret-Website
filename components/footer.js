52a232371ea985f5052321c303a1f6535f4d400e
Rishi Collins <67651571+collinsrs@users.noreply.github.com>
2022-05-15 16:27:53 -0400

    upated instagram url

--- components/footer.js
+++ components/footer.js
@@ -13,7 +13,7 @@ export default function Footer() {
             Change is possible. Vote now.
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-          <Link href='https://instagram.com/collinsforpresident'>
+          <Link href='https://instagram.com/collinsforpomfret'>
            <div className='mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0'>
            <InstagramIcon className="w-12 h-12 text-white" />
 
