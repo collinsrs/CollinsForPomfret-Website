775544439938cd5381b20e99b88a02a9d2d460be
Rishi Collins <67651571+collinsrs@users.noreply.github.com>
2022-05-16 08:58:50 -0400

    Update to fix

--- components/form.js
+++ components/form.js
@@ -28,7 +28,7 @@ export default function ContactForm() {
           if (message === "" ) {
               alert("Message not sent. Message field was left blank. Please try again.");
             } else {
-                emailjs.sendForm(process.env.EJS_SERVICE, 'template_g2xfwfd', form.current, process.env.EJS_PUBLIC_KEY)
+                emailjs.sendForm('service_z5rzdqx', 'template_g2xfwfd', form.current, 'lSmh6d1DzMROl9CYh')
                 .then((result) => {
                     console.log(result.text);
                     alert("Your message has been sent!");
