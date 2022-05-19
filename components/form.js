a3d557c35f75a5766d935e129bb9c2a54d868c9f
Rishi Collins <67651571+collinsrs@users.noreply.github.com>
2022-05-19 16:35:05 -0400

    Removed contact abilities

--- components/form.js
+++ components/form.js
@@ -70,12 +70,6 @@ export default function ContactForm() {
                         <button disabled={disabled} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={sendEmail}>
                             Send Message
                         </button>
-                        <ReCAPTCHA
-	                ref={recaptchaRef}
-	                size="invisible"
-	                sitekey={sk}
-                    onChange={onReCAPTCHAChange}
-	                />
                     </div>
                 </form>
                 <hr className="my-8" />
